830637d8d0558399b80e757c8d24ab3d262083cd
bunnyyTheFreak <hsinghalk@yahoo.com>
2020-07-18 01:31:29 +0530

    a5xelte: Refractor for FreakyOS
    Signed-off-by: bunnyyTheFreak <hsinghalk@yahoo.com>

--- freaky_a5xelte.mk
+++ freaky_a5xelte.mk
@@ -22,10 +22,32 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_l_mr1.mk)
 
 # Inherit common Lineage phone.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/freaky/config/common_full_phone.mk)
+
+# Release tools
+#TARGET_RELEASETOOLS_EXTENSIONS := $(LOCAL_PATH)
+
+# Override build user prop
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.user=bunnyy
+
+# Build Type
+CUSTOM_BUILD_TYPE := OFFICIAL
+
+# Boot animation resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+#PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+#    ro.adb.secure=0 \
+#    ro.secure=0 \
+#    ro.debuggable=1 \
+#    persist.logd.logpersistd=logcatd \
+#    persist.service.adb.enable=1 \
+#    persist.sys.usb.config=adb \
+#    ro.logd.size.stats=16M
 
 # Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := lineage_a5xelte
+PRODUCT_NAME := freaky_a5xelte
 PRODUCT_DEVICE := a5xelte
 PRODUCT_MODEL := SM-A510F
 PRODUCT_BRAND := samsung
